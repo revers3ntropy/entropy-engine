46a14906393e77958a5ac843b27cf5b82ae91619
Joseph Coppin <josephcoppin@gmail.com>
2022-04-19 21:20:51 +0100

    Updated dependencies

--- src/systems/entities/entityController.ts
+++ src/systems/entities/entityController.ts
@@ -1,13 +1,12 @@
 import {Systems} from "../../ECS/system";
-import {Scene} from "../../ECS/scene";
 
 Systems.systems.push({
     name: 'Scripts',
-    Start: (scene, canvases) => {
+    Start: (scene) => {
         scene.broadcast('Start', []);
     },
 
-    Update: (scene, canvases) => {
+    Update: (scene) => {
         scene.broadcast('Update', []);
     },
 
